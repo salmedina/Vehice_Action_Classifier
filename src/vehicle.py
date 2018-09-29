268d261992cf72dc1dfcb4cc5d2998fc2fc74305
HimaVarsha94 <varshahima1994@gmail.com>
2018-09-29 15:11:45 -0400

    vehicle class added

---
+++ src/vehicle.py
@@ -0,0 +1,8 @@
+class Vehicle():
+
+    def __init__(self):
+        self.init_slope = 0
+        self.car_points = {}
+        self.num_of_frames_covered = 0
+        self.theta = []
+        self.slopes = []
