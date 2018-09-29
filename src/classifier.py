2ac2c1c191ac4b85c4314e7b562372f294f0a23d
HimaVarsha94 <varshahima1994@gmail.com>
2018-09-29 17:41:34 -0400

    minor changes

--- src/classifier.py
+++ src/classifier.py
@@ -1,25 +1,27 @@
 import cv2
-import numpy as np
-from vehicle import Vehicle
+from vehicle import Vehicle, Global_Variables
 from utils.utils import *
-import pickle
+# import pickle
 
 
 def main():
     counter = 0  # to break after counter number of frames
-    window_size = 2
+    window_size = 15
     vehicles = {}
+    global_variables = Global_Variables()
     with open("../data/car.txt", "r") as f:
         for line in f:
             counter += 1
-            if counter > 847:
-                break
+            if counter < 0:
+                continue
+            # if counter > 500:
+                # break
             val = line.strip().split(",")
             frame_num = int(val[0])
             # store the vehicle ids, frame numbers with points
             vehicle_id = val[1]
-            x = int(val[2]) + int(val[4])//2
-            y = int(val[3]) + int(val[5])//2
+            x = int(val[2]) + int(val[4])/2
+            y = int(val[3]) + int(val[5])/2
 
             # dont check for the first window points
             if vehicle_id not in vehicles:
@@ -35,20 +37,27 @@ def main():
                 if v.num_of_frames_covered <= window_size:
                     continue
                 elif v.num_of_frames_covered == window_size+1:
-                    slope = get_vector((x, y), v, frame_num, window_size)
-                    v.init_slope = slope
+                    vector, norm = get_vector((x, y), v, frame_num, window_size)
+                    v.init_slope = vector
+                    global_variables.max_norm = max(norm, global_variables.max_norm)
+                    print(vector)
                     continue
 
-            slope = get_vector((x, y), v, frame_num, window_size)
-            theta = angle_between(slope, v.init_slope)
+            vector, norm = get_vector((x, y), v, frame_num, window_size)
+            theta = angle_between(vector, v.init_slope)
             v.theta.append(theta)
-            v.slopes.append(slope)
+            v.vectors.append(vector)
+            v.norms.append(norm)
+            global_variables.max_norm = max(norm, global_variables.max_norm)
 
     for v_id in vehicles:
-        print(v_id)
-        print(max(vehicles[v_id].theta))
-        hist_bin(vehicles[v_id].theta)
-        pickle.dump(vehicles[v_id].slopes, open('../data/slopes.pkl', 'wb'))
+        try:
+            print(v_id)
+            print(max(vehicles[v_id].theta))
+            print(global_variables.max_norm)
+            get_plot([i/global_variables.max_norm for i in vehicles[v_id].norms])
+        except:
+            continue
     cv2.destroyAllWindows()
 
 
