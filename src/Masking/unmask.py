9d69e417852fb4792e7cdd0591f58858196026ca
salmedina <salbed.medina@gmail.com>
2018-10-09 01:06:57 -0400

    Add functionality to unmask util

--- src/Masking/unmask.py
+++
@@ -1,9 +0,0 @@
-from pycocotools.coco import COCO
-import pycocotools.mask as maskUtils
-import json
-
-filename = "./mrcnn_101/VIRAT_S_050000_12_001591_001619_F_00000833.json"
-data = json.load(open(filename))
-n = len(data)
-for i in range(n):
-    val = maskUtils.decode(data[i])
