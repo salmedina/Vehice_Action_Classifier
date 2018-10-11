458a38d57a26ad36cd49cbf65c3075f8bc3e6281
HimaVarsha94 <varshahima1994@gmail.com>
2018-10-11 03:40:35 -0400

    input and right output extracted

--- src/orientation_classifier.py
+++ src/orientation_classifier.py
@@ -32,25 +32,25 @@ def parse_xml_file(filename):
 
 dir_path = '/home/hima/hima/data/Insight-MVT_Annotation_Train/'
 xml_path = './xml_data/'
+resnet_box_features = '/home/hima/hima/data/resnet_box_features/'
 resnet_model = ResNet50(weights='imagenet', include_top=False)
 
+# get resnet features
 for dir_ in os.listdir(dir_path):
     tmp_dir = dir_path + dir_ + "/"
     print(tmp_dir)
     xml_file = xml_path + dir_ + ".xml"
     boxes, orientations = parse_xml_file(xml_file)
-    import pdb; pdb.set_trace()
     for file in os.listdir(tmp_dir):
-        img = image.img_to_array(image.load_img(tmp_dir + file))
+        img = cv2.imread(tmp_dir + file)
         frame_num = int(file[-9: -4])
         frame_boxes = boxes[frame_num - 1]
         frame_orientations = orientations[frame_num - 1]
         for i, box in enumerate(frame_boxes):
             img_rows = img[get_int(box['top']):get_int(box['top'])+get_int(box['height'])]
             final_img = img_rows[:, get_int(box['left']):get_int(box['left'])+get_int(box['width'])]
-            print(final_img, frame_orientations[i])
-            # cv2.imshow(final_img)
-            # img_data = image.img_to_array(final_img)
-            # img_data = np.expand_dims(img_data, axis=0)
-            # img_data = preprocess_input(img_data)
-            # resnet_feature = resnet_model.predict(img_data)
+            # pre_data = preprocess_input(np.expand_dims(final_img, axis=0))
+            resized_img = cv2.resize(final_img, (244, 244))
+            r_f = resnet_model.predict(resized_img.reshape(1, 244, 244, 3)) # input
+            output = orientations[i]
+            np.save(open(resnet_box_features+dir_+"|"+file+"|"+str(i), 'w'), r_f)
