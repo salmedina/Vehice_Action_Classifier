a5e3a95a4e04b6a75918cc2839989a8763cb1e28
tyhu <benken.tyhu@gmail.com>
2018-10-12 00:27:24 -0400

    car orientation training

---
+++ src/pytorch/datasets.py
@@ -0,0 +1,42 @@
+import sys
+import os
+from PIL import Image
+import numpy as np
+import os.path as osp
+
+import torch
+from torch.utils.data import Dataset
+
+
+def read_image(img_path):
+    """Keep reading image until succeed.
+    This can avoid IOError incurred by heavy IO process."""
+    got_img = False
+    if not osp.exists(img_path):
+        raise IOError("{} does not exist".format(img_path))
+    while not got_img:
+        try:
+            img = Image.open(img_path).convert('RGB')
+            got_img = True
+        except IOError:
+            print("IOError incurred when reading '{}'. Will redo. Don't worry. Just chill.".format(img_path))
+            pass
+    return img
+
+
+
+class CarOrientationDataset(Dataset):
+
+    def __init__(self, imgfnlist, degreelist, binlist, transform=None):
+        self.imgfnlist, self.degreelist, self.binlist = imgfnlist, degreelist, binlist
+        self.transform = transform
+
+    def __getitem__(self, idx):
+        imgfn, degree, binn = self.imgfnlist[idx], self.degreelist[idx], self.binlist[idx]
+        img = read_image(imgfn)
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, degree, binn
+
+    def __len__(self):
+        return len(self.imgfnlist)
---
+++ src/pytorch/main.py
@@ -0,0 +1,88 @@
+import sys
+import argparse
+from myconfig import *
+from datasets import CarOrientationDataset
+from models import ResNet50
+
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+from torch.optim import lr_scheduler, Adam
+import torchvision.transforms as T
+
+def test(data_loader, model):
+    model.eval()
+
+    matched,total = 0,0
+    for imgs, _, degree_bins in data_loader:
+        imgs, degree_bins = Variable(imgs.cuda()), Variable(degree_bins.cuda())
+        out = model(imgs)
+        _, predicted = torch.max(out.data, 1)
+        matched+=predicted.eq(degree_bins.data).sum()
+        total+=degree_bins.size(0)
+    acc = 100.*matched/total
+    return acc
+
+
+### arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--lr', type=float, default=0.005, help='Initial learning rate.')
+parser.add_argument('--size', type=int, default=50, help='Size of reshaped image')
+parser.add_argument('--weight_decay', type=float, default=5e-4, help='Weight decay (L2 loss on parameters).')
+parser.add_argument('--epochs', type=int, default=100, help='max num of epochs')
+parser.add_argument('--batch_size', type=int, default=16, help='batch size')
+parser.add_argument('--workers', type=int, default=4, help='num of workers for data loading')
+args = parser.parse_args()
+
+### transofrm
+transform_train = T.Compose([
+        T.Resize((args.size, args.size)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+transform_test = T.Compose([
+        T.Resize((args.size, args.size)),
+        T.ToTensor(),
+        T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
+
+### Data preparation
+lst = [l for l in open(cfg_annot_fn, 'r')]
+lst.pop(0)
+train_dataset = CarOrientationDataset(*cfg_extract_data_split(lst, cfg_train_views), transform=transform_train)
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, num_workers=args.workers, drop_last=True)
+valid_dataset = CarOrientationDataset(*cfg_extract_data_split(lst, cfg_valid_views), transform=transform_test)
+valid_loader = DataLoader(valid_dataset, batch_size=args.batch_size, num_workers=args.workers, drop_last=False, shuffle=False)
+
+
+### model, optimizer, and loss
+model = ResNet50(16).cuda()
+optimizer = Adam(model.parameters(),
+        lr=args.lr,
+        weight_decay=args.weight_decay)
+criterion = nn.CrossEntropyLoss()
+
+acc = test(valid_loader,model)
+print('initial acc:',acc)
+for ep in range(args.epochs):
+    total_loss = 0
+    model.train()
+    for imgs, _, degree_bins in train_loader:
+        imgs, degree_bins = Variable(imgs.cuda()), Variable(degree_bins.cuda())
+        out = model(imgs)
+        loss = criterion(out, degree_bins)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        total_loss+=loss.data[0]
+    if (ep+1)%1==0:
+        print('epoch: {}\t loss: {:.6f}'.format(ep,total_loss/len(train_loader)))
+        acc = test(valid_loader,model)
+        print('Validation acc: {:.2f}'.format(acc))
+
+
+torch.save(model.state_dict(),'model_state.pt')
