8e4e7e29d315c8fcc493107038eca0c7bc0fba9f
salmedina <salbed.medina@gmail.com>
2018-10-23 10:21:08 -0400

    Add lr and margin to config in scene clf

--- src/scene_classifier/main.py
+++ src/scene_classifier/main.py
@@ -1,3 +1,4 @@
+import argparse
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
 from datasets import SiameseNetworkDataset
@@ -32,9 +33,10 @@ Config.testing_dir = ""
 Config.train_batch_size = 64
 Config.train_number_epochs = 100
 Config.num_workers = 16
+Config.contrastive_margin = 1.0
+Config.learning_rate = 0.0005
 
 # Data
-
 train_dataset = SiameseNetworkDataset(imageFolderDataset=dset.ImageFolder(root=Config.training_dir),
                                         transform=transforms.Compose([transforms.Resize((100,100)),
                                                                       transforms.ToTensor()
@@ -67,26 +69,27 @@ print(example_batch[2].numpy())
 
 # Model declaration
 net = SiameseNetwork().cuda()
-criterion = ContrastiveLoss()
-optimizer = Adam(net.parameters(),lr = 0.0005)
+criterion = ContrastiveLoss(margin=Config.contrastive_margin)
+optimizer = Adam(net.parameters(),lr = Config.learning_rate)
 
 # Training
 counter = []
 loss_history = []
-iteration_number= 0
+iteration_number = 0
 
 for epoch in range(0,Config.train_number_epochs):
     for i, data in enumerate(train_dataloader,0):
-        img0, img1 , label = data
-        img0, img1 , label = Variable(img0).cuda(), Variable(img1).cuda() , Variable(label).cuda()
-        output1,output2 = net(img0,img1)
+        img0, img1, label = data
+        img0, img1, label = Variable(img0).cuda(), Variable(img1).cuda() , Variable(label).cuda()
+        output1, output2 = net(img0, img1)
         optimizer.zero_grad()
-        loss_contrastive = criterion(output1,output2,label)
+        loss_contrastive = criterion(output1, output2, label)
         loss_contrastive.backward()
         optimizer.step()
         if i %10 == 0 :
-            print("Epoch number {}\n Current loss {}\n".format(epoch,loss_contrastive.data[0]))
+            print("Epoch number {}\n Current loss {}\n".format(epoch, loss_contrastive.data[0]))
             iteration_number +=10
             counter.append(iteration_number)
             loss_history.append(loss_contrastive.data[0])
+
 show_plot(counter,loss_history)
